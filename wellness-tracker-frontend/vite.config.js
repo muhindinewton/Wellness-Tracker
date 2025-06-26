b2f921bb8c6551d03016a90d9143da9ac79369fa
Presbury-Wamae <wamaepresbury034@gmail.com>
2025-06-26 10:43:13 +0300

    add preview configuration to vite.config.js for external access

--- wellness-tracker-frontend/vite.config.js
+++ wellness-tracker-frontend/vite.config.js
@@ -7,7 +7,12 @@ export default defineConfig({
   base: '/', // 👈 important for Flask to serve assets correctly
   plugins: [react(), tailwindcss()],
   build: {
-    outDir: 'dist', // default is fine
+    outDir: 'dist',
     emptyOutDir: true,
+  },
+  preview: {
+    port: process.env.PORT, // Use the port Render provides
+    host: true, // Bind to 0.0.0.0 (external access)
+    allowedHosts: ['wellness-tracker-9b8i.onrender.com'], // Allow Render to access it
   },
 })
